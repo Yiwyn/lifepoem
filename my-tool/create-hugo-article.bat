4121494d21aad02a047a1f00466f7334cbaa5752
Yiwyn <5597104@qq.com>
2024-09-29 18:02:35 +0800

    uml

--- my-tool/create-hugo-article.bat
+++ my-tool/create-hugo-article.bat
@@ -2,12 +2,19 @@
 setlocal enabledelayedexpansion
 
 set "fileName=%1"
+
+if "%fileName%"=="" (
+    echo fileName Not Empty!
+     exit /b 1
+) 
+
+
 set "articlePath=content\posts\%fileName%.md"
 
 if exist "!articlePath!" (
     echo Article already exists!
 ) else (
-  cd ..
-   hugo new  content  content/posts/%fileName%.md"
-    echo Article created: !articlePath!
+   cd ..
+   hugo new content  content/posts/%fileName%.md"
+   echo Article created: !articlePath!
 )
